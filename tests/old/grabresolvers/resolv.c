869a9451f0cc1b1c1a2a08e3bfb218d1f2a785ef
the-scientist <tofu@rootstorm.com>
2021-11-05 14:50:49 -0600

    move to old test

--- tests/old/grabresolvers/resolv.c
+++ tests/old/grabresolvers/resolv.c
@@ -19,11 +19,24 @@ const char* g_commands[ NCOMMANDS_J0LT ] = {
     "wget -O /tmp/resolv.txt https://raw.githubusercontent.com/thesc1entist/j0lt/main/j0lt-resolv.txt"
 };
 
+void Red(void) {
+    printf("\033[1;31m");
+}
+
+void Green(void) {
+    printf("\033[0;32m");
+}
+
+void Reset(void) {
+    printf("\033[0m");
+}
+
 int main(int argc, char** argv)
 {
     FILE* fptr;
     char lineptr[ MAXREAD_J0LT ];
     size_t nread;
+
     system(g_commands[ COMMAND_WGET_J0LT ]); // grab resolv list
     fptr = fopen(g_commands[ COMMAND_PATH_J0LT ], "r");
 
@@ -32,14 +45,21 @@ int main(int argc, char** argv)
         exit(EXIT_FAILURE);
     }
 
+    Green( );
+    printf("+ resolv list saved to %s\n", g_commands[ COMMAND_PATH_J0LT ]);
+    Reset( );
+
     while (fgets(lineptr, MAXREAD_J0LT, fptr) != NULL) {
         if (lineptr[ 0 ] == '#')
             continue;
         nread = strlen(lineptr);
         lineptr[ nread - 1 ] = '\0';
-        puts(lineptr);
+        // puts(lineptr);
     }
 
+    Red( );
+    printf("+ removing resolv list from %s\n", g_commands[ COMMAND_PATH_J0LT ]);
+    Reset( );
     system(g_commands[ COMMAND_RM_J0LT ]); // remove resolv list
     fclose(fptr);
 
